2223b84672ee06ba83d8c1c5ca66dbe998840285
V <evko9975@colorado.edu>
2026-01-03 22:20:20 -0700

    Update config.js

--- charadex-v1.0-1.5.5-develop/styles/js/config.js
+++ charadex-v1.0-1.5.5-develop/styles/js/config.js
@@ -15,8 +15,8 @@ let charadex = {};
 /* Any preview links will still show Charadex's information
 /* ==================================================================== */
 charadex.site = {
-  title: "Charadex",
-  url: "https://charadex-team.github.io/charadex-v1.0/",
+  title: "Rureks",
+  url: "https://rureks.github.io",
   description: `A tool for organizing small ARPGs and species.`
 }
 
